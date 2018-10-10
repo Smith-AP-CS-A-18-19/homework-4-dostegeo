16d4f7e7df044e480914cb8810c7910705611887
George Doster <dostegeo@berkeleyprep.org>
2018-10-10 14:42:44 -0400

    Here

--- Homework4.java
+++ Homework4.java
@@ -1,17 +1,23 @@
 public class Homework4 {
-
+//George Doster
 	/* Finish the constructor and create any necessary instance
 	 * variables
 	 */
+	 private String letters;
+	 private int numbers;
 	public Homework4(String s, int i) {
-
+letters = s;
+numbers = i;
 	}
 
 	/* Return the product of the stored number and the
 	 * provided double. Cast the number after the
 	 * multiplication
 	 */
+
 	public int multiply(double d) {
+		double m=d*numbers;
+		return (int) m;
 
 	}
 
@@ -19,27 +25,33 @@ public class Homework4 {
 	 * product by the stored number
 	 */
 	public double sumProduct(int a, double b) {
-
+double sum = a+b;
+double p= sum*numbers;
+return p;
 	}
 
 	/* Return the remainder of the first number divided by
 	 * the integer portion of the provided double
 	 */
 	 public int remainder(double d) {
-
+int integer= (int)d;
+int remainder = numbers%integer;
+return remainder;
 	}
 
 	/* Return the length of the stored String
 	 */
 	public int getLength() {
-
+int l = letters.length();
+return l;
 	}
 
 	/* Return the stored String followed by the provided
 	 * String. Put a space between the two Strings
 	 */
 	public String concat(String s) {
-
+String ss= letters + " " + s;
+return ss;
 	}
 
 	public static void main(String[] args) {
@@ -47,14 +59,14 @@ public class Homework4 {
 
 		Homework4 hw4 = new Homework4("Hello, World!", 5);
 
-		if (hw4.multiply(2.5) == 12.25) {
+		if (hw4.multiply(2.5) == 12) {
 			System.out.println("Pass 1");
 		} else {
 			System.out.println("Fail 1");
 			exitCode += 1;
 		}
 
-		if (hw4.sumProduct(2, 2.5) == 20.25) {
+		if (hw4.sumProduct(2, 2.5) == 22.5) {
 			System.out.println("Pass 2");
 		} else {
 			System.out.println("Fail 2");
@@ -75,7 +87,7 @@ public class Homework4 {
 			exitCode += 1;
 		}
 
-		if (hw4.concat("Goodbye!").equals("Hello World! Goodbye!")) {
+		if (hw4.concat("Goodbye!").equals("Hello, World! Goodbye!")) {
 			System.out.println("Pass 5");
 		} else {
 			System.out.println("Fail 5");
